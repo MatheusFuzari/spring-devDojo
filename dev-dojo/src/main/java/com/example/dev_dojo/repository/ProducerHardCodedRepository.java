52cadb12cfef7be84c221f47f2b49ae395ba16ba
MatheusFuzari <matheus.fuzari004@gmail.com>
2025-01-22 21:58:26 -0300

    aula 34

--- dev-dojo/src/main/java/com/example/dev_dojo/repository/ProducerHardCodedRepository.java
+++ dev-dojo/src/main/java/com/example/dev_dojo/repository/ProducerHardCodedRepository.java
@@ -14,34 +14,32 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class ProducerHardCodedRepository {
-    private static final List<Producer> PRODUCERS = new ArrayList<>();
+
+    private final ProducerData producerData;
 
     @Qualifier(value = "connectionMongo")
     private final Connection connection;
 
-    static {
-        PRODUCERS.addAll(List.of(Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build(), Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build(), Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build(), Producer.builder().id(4L).name("Ghibli Studios").createdAt(LocalDateTime.now()).build()));
-    }
 
     public List<Producer> findAll() {
-        return PRODUCERS;
+        return producerData.getProducers();
     }
 
     public List<Producer> findByName(String name) {
-        return PRODUCERS.stream().filter(a -> a.getName().equalsIgnoreCase(name)).toList();
+        return producerData.getProducers().stream().filter(a -> a.getName().equalsIgnoreCase(name)).toList();
     }
 
     public Optional<Producer> findById(Long id) {
-        return PRODUCERS.stream().filter(a -> a.getId().equals(id)).findFirst();
+        return producerData.getProducers().stream().filter(a -> a.getId().equals(id)).findFirst();
     }
 
     public Producer save(Producer producer) {
-        PRODUCERS.add(producer);
+        producerData.getProducers().add(producer);
         return producer;
     }
 
     public void delete(Producer producer) {
-        PRODUCERS.remove(producer);
+        producerData.getProducers().remove(producer);
     }
 
     public void update(Producer producer) {
